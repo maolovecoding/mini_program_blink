f023e511f716bca9479e2d9745341c9cdef6ea37
maolovecoding <1339065735@qq.com>
2021-09-16 07:50:15 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
 // const config = {
-//   appkey:"XlPyACBYSGylPXnZ",
-//   baseUrl:"https://bls.talelin.com/v1"
+//   appkey:"",
+//   baseUrl:""
 // }
 // export default config;
 
 // 直接导出 baseUrl 以及 appkey
 
-// export const appkey = "XlPyACBYSGylPXnZ";
-export const appkey = "RdshydjBvcYZhMZC";
-// export const baseUrl = "https://bls.talelin.com/v1/";
-export const baseUrl = "http://bl.talelin.com/v1/";
+// export const appkey = "";
+export const appkey = "";
+// export const baseUrl = "/";
+export const baseUrl = "/";
 
 
 
